3fb8dec40d2d69f57f9c8c9cd369510a74c5138d
IceVan <bozga@loken.pl>
2020-06-24 15:36:53 +0200

    zapomniałem o kodowaniu

--- demo/src/main/java/ice/demo/test/demo/service/CarsServiceImpl.java
+++ demo/src/main/java/ice/demo/test/demo/service/CarsServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
@@ -45,7 +46,8 @@ public class CarsServiceImpl implements CarsService {
 
             while ((line = br.readLine()) != null) {
 
-                String[] car = line.split(cvsSplitBy);
+                String utfLine = new String(line.getBytes(), StandardCharsets.UTF_8);
+                String[] car = utfLine.split(cvsSplitBy);
 
                 if(car.length == 4 && car[0] != null && !car[0].isEmpty() && car[3] != null && car[3].equalsIgnoreCase(color)){
                     Date date =new Date(format.parse ( car[2] ).getTime());
